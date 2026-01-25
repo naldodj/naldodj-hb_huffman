5bce18bfebf422da54f3bd42f0072597d6d9974d
naldodj <marinaldo.jesus@gmail.com>
2026-01-24 21:33:06 -0300

    2026-01-25 00:32 UTC naldodj (Add binary Huffman compression and logging to tests)

---
+++ src/tst/go64.bat
@@ -0,0 +1,8 @@
+@setlocal
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+    F:\harbour_msvc\bin\win\msvc64\hbmk2 huffmannode_tst.hbp -comp=msvc64
+    if EXIST .\huffmannode_tst.exe (
+        cmd /c upx.exe .\huffmannode_tst.exe
+        certutil -hashfile .\huffmannode_tst.exe SHA256 > huffmannode_tst.sh256
+    )
+@endlocal
